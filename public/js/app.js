dd8b2cbf1afc978b2b4e9e5a4aac81c4599a1dd5
allen-soler <45588179+allen-soler@users.noreply.github.com>
2022-03-15 20:28:13 +0100

    adding web server

--- public/js/app.js
+++ public/js/app.js
@@ -1,22 +1,33 @@
-fetch('http://localhost:3000/weather?address=geneva').then((response) => {
-    response.json().then((data) => {
-        let main = document.getElementById("container");
-        let div = document.createElement("DIV");
-        let h1 = document.createElement("h1");
-        if (data.error) {
-            h1.innerText = data.error;
-            main.appendChild(div);
-            div.appendChild(h1);
-        }
-        else {
-            let p = document.createElement("p");
+const weatherCB = (name) => {
+    fetch(`http://localhost:3000/weather?address=${name}`).then((response) => {
+        response.json().then((data) => {
+            //let main = document.getElementById("container");
+            let h1 = document.getElementById("location");
+            let p = document.getElementById("forecast");
+            if (data.error) {
+                h1.textContent = "" + data.error;
+                p.textContent = "";
+                //main.appendChild(div);
+                //div.appendChild(h1);
+            }
+            else {
+                //let p = document.createElement("p");
 
-            h1.innerText = data.location;
-            p.innerText = data.forecast;
-            div.classList.add("WeatherInfo")
-            main.appendChild(div);
-            div.appendChild(h1);
-            div.appendChild(p);
-        }
+                h1.textContent = data.location;
+                p.textContent = data.forecast;
+                //div.classList.add("WeatherInfo")
+                //main.appendChild(div);
+                //div.appendChild(h1);
+                //div.appendChild(p);
+            }
+        })
     })
+}
+
+let form = document.getElementById('formSearch');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let data = document.querySelector('input').value;
+    weatherCB(data);
 })
